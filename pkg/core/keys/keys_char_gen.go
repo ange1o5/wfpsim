7390c080d57767f5a1c78eeb99bfb095ceef26f4
Dmytro Kalitin <dimon.ka2002@gmail.com>
2025-05-11 22:43:51 +0300

    add Skirk key.

--- pkg/core/keys/keys_char_gen.go
+++ pkg/core/keys/keys_char_gen.go
@@ -81,6 +81,7 @@ const (
 	Sethos
 	Shenhe
 	Sigewinne
+	Skirk
 	Sucrose
 	Tartaglia
 	Thoma
@@ -396,6 +397,10 @@ func init() {
 	charNames[Sigewinne] = "sigewinne"
 	charPrettyName[Sigewinne] = "Sigewinne"
 	CharKeyToEle[Sigewinne] = attributes.Hydro
+
+	charNames[Skirk] = "skirk"
+	charPrettyName[Skirk] = "Skirk"
+	CharKeyToEle[Skirk] = attributes.Cryo
 
 	charNames[Sucrose] = "sucrose"
 	charPrettyName[Sucrose] = "Sucrose"
